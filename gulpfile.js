6b3151e4accb7f63c157f1d6bab7975a0d6d12e2
Rommel Castro <rommelxcastro@gmail.com>
2015-08-22 11:50:26 -0600

    gulp file

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    babelify = require('babelify'),
+    source = require('source');
+
+gulp.task('build', function buildTask() {
+    browserify({
+        entries: 'app.js',
+        extensions: ['.jsx'],
+        debug: true
+    })
+        .transform(babelify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['build']);
